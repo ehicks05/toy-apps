1e5156ea30a9dd6bd6b694757c40f3baa2057c0e
Eric Hicks <ehicks05@yahoo.com>
2021-05-24 23:18:36 -0400

    add chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from "react";
 import "./App.css";
 import { useQuery } from "react-query";
+import { useTable } from "react-table";
 
 import { usConfirmed, queryOptions } from "./constants";
 import { getData } from "./utils";
@@ -11,22 +13,97 @@ function App() {
     queryOptions
   );
 
+  // const columns = React.useMemo(
+  //   () => [
+  //     {
+  //       Header: "Combined_Key",
+  //       accessor: "Combined_Key", // accessor is the "key" in the data
+  //     },
+  //     {
+  //       Header: "Province_State",
+  //       accessor: "Province_State",
+  //     },
+  //   ],
+  //   []
+  // );
+
+  const filter = (key) => {
+    console.log(
+      key +
+        ": " +
+        ["Admin2", "Province_State", "Country_Region"].includes(key) +
+        ", " +
+        !isNaN(key.charAt(0))
+    );
+    return (
+      ["Admin2", "Province_State", "Country_Region"].includes(key) ||
+      !isNaN(key.charAt(0))
+    );
+  };
+
+  const columns = React.useMemo(
+    () =>
+      Object.keys(data?.length > 0 ? data[0] : {})
+        .filter(filter)
+        .map((key) => ({
+          Header: key,
+          accessor: key,
+        })),
+    [data]
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
   if (isError) return <div>Error...</div>;
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <ul>
-            {data
-              .filter((row) => row.Province_State === "New Jersey")
-              .map((row) => (
-                <li key={row.Combined_Key}>{JSON.stringify(row)}</li>
+      <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: "solid 3px red",
+                    background: "aliceblue",
+                    color: "black",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {column.render("Header")}
+                </th>
               ))}
-          </ul>
-        </div>
-      </header>
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: "10px",
+                        border: "solid 1px gray",
+                        background: "papayawhip",
+                      }}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,9 @@
 import Papa from "papaparse";
 
+const filter = (row) =>
+  row.Province_State === "New Jersey" &&
+  ["Hunterdon", "Somerset"].includes(row.Admin2);
+
 const getData = async (url) => {
   const response = await fetch(url);
   const text = await response.text();
@@ -7,7 +11,7 @@ const getData = async (url) => {
   const parseResult = await Papa.parse(text, {
     header: true,
   });
-  return parseResult.data;
+  return parseResult.data.filter(filter);
 };
 
 export { getData };
