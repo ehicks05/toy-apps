9f99481e386396b7f59ba6f84356abc6bbca53cf
Eric Hicks <ehicks05@yahoo.com>
2021-05-27 18:50:45 -0400

    fix links

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const usConfirmed =
-  "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
 const usDeaths =
-  "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv";
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv";
 
 const INFECTION_DURATION = 14;
 
