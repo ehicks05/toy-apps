17f766cc3fc5a290837cb03d559c46014f383ffe
Eric Hicks <ehicks05@yahoo.com>
2021-05-27 18:51:01 -0400

    compute nj totals

--- src/utils.js
+++ src/utils.js
@@ -14,9 +14,7 @@ const pretty = (input) =>
     maximumFractionDigits: 3,
   }).format(input);
 
-const filter = (row) =>
-  row.Province_State === "New Jersey" &&
-  ["Hunterdon", "Somerset"].includes(row.Admin2);
+const isNJ = (row) => row.Province_State === "New Jersey";
 
 const mapConfirmed = (row) => {
   const { Admin2, Province_State } = row;
@@ -62,12 +60,12 @@ const getData = async () => {
   });
 
   const usConfirmedGrouped = _.groupBy(
-    usConfirmedData.data.filter(filter).map(mapConfirmed).flat(),
+    usConfirmedData.data.filter(isNJ).map(mapConfirmed).flat(),
     "date"
   );
 
   const usDeathsGrouped = _.groupBy(
-    usDeathsData.data.filter(filter).map(mapDeaths).flat(),
+    usDeathsData.data.filter(isNJ).map(mapDeaths).flat(),
     "date"
   );
 
@@ -75,10 +73,12 @@ const getData = async () => {
     date,
     hunterdonConfirmed: data.find((r) => r.Admin2 === "Hunterdon").confirmed,
     somersetConfirmed: data.find((r) => r.Admin2 === "Somerset").confirmed,
+    njConfirmed: _.sumBy(data, (row) => row.confirmed),
     hunterdonDeaths: usDeathsGrouped[date].find((r) => r.Admin2 === "Hunterdon")
       .deaths,
     somersetDeaths: usDeathsGrouped[date].find((r) => r.Admin2 === "Somerset")
       .deaths,
+    njDeaths: _.sumBy(usDeathsGrouped[date], (row) => row.deaths),
   }));
 
   const resultsWithComputedData = result.map((row) => {
@@ -93,19 +93,26 @@ const getData = async () => {
       result.find((row) => row.date === formattedDate)?.hunterdonConfirmed || 0;
     const hunterdonActive = row.hunterdonConfirmed - hunterdonCasesToSubtract;
 
+    const njCasesToSubtract =
+      result.find((row) => row.date === formattedDate)?.njConfirmed || 0;
+    const njActive = row.njConfirmed - njCasesToSubtract;
+
     const somersetActivePercent = pretty(
       (100 * somersetActive) / POPULATIONS.SOMERSET
     );
     const hunterdonActivePercent = pretty(
       (100 * hunterdonActive) / POPULATIONS.HUNTERDON
     );
+    const njActivePercent = pretty((100 * njActive) / POPULATIONS.NEW_JERSEY);
 
     return {
       ...row,
       somersetActive,
       hunterdonActive,
+      njActive,
       somersetActivePercent,
       hunterdonActivePercent,
+      njActivePercent,
     };
   });
 
