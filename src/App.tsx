fec51b74041e3797e69ab7971bb9b31e3c4d43e0
Eric Hicks <ehicks05@yahoo.com>
2022-05-29 19:27:23 -0400

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,19 @@ const App = () => {
   const [guesses, setGuesses] = useState<string[]>([]);
   const [input, setInput] = useState<string>('');
 
-
   return (
     <div className="flex flex-col items-center gap-2 p-4">
-      <h1 className='text-4xl mb-4'>Eordle</h1>
-      {[...new Array(6)].map(row => 
-        <div className='flex gap-2'>
-          {[...new Array(5)].map(col => 
-          <input maxLength={1} className='w-10 h-10 text-center bg-neutral-900 border border-neutral-500 rounded' />)}
-          </div>)}
+      <h1 className="text-4xl mb-4">Eordle</h1>
+      {[...new Array(6)].map((row) => (
+        <div className="flex gap-2">
+          {[...new Array(5)].map((col) => (
+            <input
+              maxLength={1}
+              className="w-10 h-10 text-center bg-neutral-900 border border-neutral-500 rounded"
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
