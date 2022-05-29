70a480389539be09db8bbeeed4a40dd186268e54
Eric Hicks <ehicks05@yahoo.com>
2022-05-29 19:25:59 -0400

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ const App = () => {
 
 
   return (
-    <div className="flex flex-col items-center gap-4 p-4">
-      <h1 className='text-4xl'>Eordle</h1>
+    <div className="flex flex-col items-center gap-2 p-4">
+      <h1 className='text-4xl mb-4'>Eordle</h1>
       {[...new Array(6)].map(row => 
-        <div className='flex gap-4'>
+        <div className='flex gap-2'>
           {[...new Array(5)].map(col => 
-          <input maxLength={1} className='w-8 h-8 p-2 bg-slate-600' />)}
+          <input maxLength={1} className='w-10 h-10 text-center bg-neutral-900 border border-neutral-500 rounded' />)}
           </div>)}
     </div>
   );
