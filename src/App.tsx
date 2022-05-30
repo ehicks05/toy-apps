0490ba95c43fc32fdee95150c060c42eba226009
Eric Hicks <ehicks05@yahoo.com>
2022-05-30 09:53:06 -0400

    mostly working kb handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,29 @@ const DEFAULT_BOARD = [...new Array(6)].map(() => [...DEFAULT_ROW]);
 
 const App = () => {
   const word = 'valor';
-  const [guesses, setGuesses] = useState(DEFAULT_BOARD);
+  const [board, setBoard] = useState(DEFAULT_BOARD);
   const [i, setI] = useState(0);
   const [j, setJ] = useState(0);
 
-  const updateGuesses = (val) => guesses.map((guess, ii) => guess.map((letter, jj) => {
-    if (ii === i && jj === j) return val;
-    return letter;
-  }));
+  const updateGuesses = (val, letterIndex) =>
+    board.map((guess, ii) => guess.map((letter, jj) => {
+      if (ii === i && jj === letterIndex) return val;
+      return letter;
+    }));
 
   const handleKey: IKeyboardEventHandler<EventTarget> = (e: KeyboardEvent) => {
     const { key } = e;
     console.log(key);
 
-    if (key === 'Enter' && guesses[i].length === 5) {
+    if (key === 'Enter' && j === 5) {
       // submit guess. todo check a word library
       setI((i) => i + 1);
+      setJ(0);
     } else if (key === 'Backspace' && j > 0) {
+      setBoard(updateGuesses('', j - 1));
       setJ((j) => j - 1);
-      setGuesses(updateGuesses(''));
-    } else if (/^[a-zA-Z]/.test(key) && j < 5) {
-      setGuesses(updateGuesses(key));
+    } else if (/^[a-zA-Z]$/.test(key) && j < 5) {
+      setBoard(updateGuesses(key, j));
       setJ((j) => j + 1);
     }
   };
@@ -37,14 +39,14 @@ const App = () => {
     <div className="flex flex-col items-center gap-2 p-4">
       <h1 className="text-4xl mb-4">Eordle</h1>
 
-      {guesses.map((guess, a) => (
+      {board.map((guess, a) => (
         <div key={a} className="flex gap-2">
           {guess.map((letter, b) => (
             <div
               key={b}
               className="flex items-center justify-center w-10 h-10 bg-neutral-900 border border-neutral-500 rounded"
             >
-              {letter}
+              {letter.toUpperCase()}
             </div>
           ))}
         </div>
@@ -52,7 +54,7 @@ const App = () => {
 
       <pre className="text-xs">
         {JSON.stringify(
-          { guesses, 'guesses[i].length': guesses[i].length },
+          { i, j, guesses: board, 'guesses[i].length': board[i].length },
           null,
           2,
         )}
