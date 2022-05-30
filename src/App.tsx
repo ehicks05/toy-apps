9f0a0fafae61b4c1de72cd4c89d64fb8dd68da96
Eric Hicks <ehicks05@yahoo.com>
2022-05-29 21:16:18 -0400

    more wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { IKeyboardEventHandler, useKeyboardEvent } from '@react-hookz/web'; // cjs
 
-const DEFAULT_GUESS = ['', '', '', '', ''];
 const DEFAULT_ROW = [...new Array(5)].map(() => '');
-const DEFAULT_BOARD = [...new Array(6)].map(() => [...DEFAULT_GUESS]);
+const DEFAULT_BOARD = [...new Array(6)].map(() => [...DEFAULT_ROW]);
 
 const App = () => {
   const word = 'valor';
-  const [guesses, setGuesses] = useState<string[][]>([[]]);
+  const [guesses, setGuesses] = useState(DEFAULT_BOARD);
   const [i, setI] = useState(0);
   const [j, setJ] = useState(0);
+
+  const updateGuesses = (val) => guesses.map((guess, ii) => guess.map((letter, jj) => {
+    if (ii === i && jj === j) return val;
+    return letter;
+  }));
+
   const handleKey: IKeyboardEventHandler<EventTarget> = (e: KeyboardEvent) => {
     const { key } = e;
     console.log(key);
@@ -17,39 +22,31 @@ const App = () => {
     if (key === 'Enter' && guesses[i].length === 5) {
       // submit guess. todo check a word library
       setI((i) => i + 1);
-    } else if (key === 'Backspace' && guesses[i].length > 0) {
-      guesses[i][j] = '';
+    } else if (key === 'Backspace' && j > 0) {
       setJ((j) => j - 1);
-    } else if (/^[a-zA-Z]/.test(key) && guesses[i].length < 5) {
-      guesses[i][j] = key;
-      setGuesses()
+      setGuesses(updateGuesses(''));
+    } else if (/^[a-zA-Z]/.test(key) && j < 5) {
+      setGuesses(updateGuesses(key));
       setJ((j) => j + 1);
     }
   };
 
   useKeyboardEvent(true, handleKey);
 
-  const displayGuesses = [
-    ...guesses,
-    ...[...new Array(6 - guesses.length)].map(() => [...DEFAULT_GUESS]),
-  ];
-
   return (
     <div className="flex flex-col items-center gap-2 p-4">
       <h1 className="text-4xl mb-4">Eordle</h1>
 
-      {displayGuesses.map((guess, a) => (
+      {guesses.map((guess, a) => (
         <div key={a} className="flex gap-2">
-          {[...guess, ...[...new Array(5 - guess.length)].map(() => '')].map(
-            (letter, b) => (
-              <div
-                key={b}
-                className="flex items-center justify-center w-10 h-10 bg-neutral-900 border border-neutral-500 rounded"
-              >
-                {letter}
-              </div>
-            )
-          )}
+          {guess.map((letter, b) => (
+            <div
+              key={b}
+              className="flex items-center justify-center w-10 h-10 bg-neutral-900 border border-neutral-500 rounded"
+            >
+              {letter}
+            </div>
+          ))}
         </div>
       ))}
 
@@ -57,7 +54,7 @@ const App = () => {
         {JSON.stringify(
           { guesses, 'guesses[i].length': guesses[i].length },
           null,
-          2
+          2,
         )}
       </pre>
     </div>
