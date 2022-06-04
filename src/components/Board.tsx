08dbabaaabb22cd4121acb69fd6a5073b16e00da
Eric Hicks <ehicks05@yahoo.com>
2022-06-04 16:41:09 -0400

    reduce gap

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -42,9 +42,9 @@ interface BoardProps {
   boardEffects: string[];
 }
 const Board = ({ board, boardEffects }: BoardProps) => (
-  <div className="flex flex-col h-full justify-center gap-2">
+  <div className="flex flex-col h-full justify-center gap-1">
     {board.map((row, a) => (
-      <div key={a} className={`flex gap-2 ${boardEffects[a]}`}>
+      <div key={a} className={`flex gap-1 ${boardEffects[a]}`}>
         {row.map((cell, b) => (
           <Cell key={b} letter={cell.letter} result={cell.result} index={b} />
         ))}
