0229690e42b3897b9322585e96b8638cdde436b5
Eric Hicks <ehicks05@yahoo.com>
2022-11-21 21:51:39 -0500

    formatting

--- src/app/TransactionHistory/components/GeneralInfo.tsx
+++ src/app/TransactionHistory/components/GeneralInfo.tsx
@@ -1,6 +1,7 @@
-import { ByteString } from "../../../core-components";
-import { SignatureWithTransaction } from "../../../types/types";
-import { blockTimeToISO, nf } from "../../utils";
+import { ByteString } from '../../../core-components';
+import { SignatureWithTransaction } from '../../../types/types';
+import { nf } from '../../utils';
+import { format } from 'date-fns';
 
 interface GeneralInfoProps {
   transaction: SignatureWithTransaction;
@@ -14,18 +15,17 @@ const GeneralInfo = ({
     transaction: { message },
   } = transaction;
 
+  const blockDatetime = {
+    date: format(new Date((blockTime || 0) * 1000), 'yyyy-MM-dd'),
+    time: format(new Date((blockTime || 0) * 1000), 'hh:mm:ssa'),
+  };
+
   return (
     <div className="flex flex-col justify-items-start gap-2 bg-sky-900 p-2">
       <div>
         <div>
-          <div>{blockTimeToISO(blockTime || 0).slice(0, 10)}</div>
-          <div>{blockTimeToISO(blockTime || 0).slice(11)}</div>
-        </div>
-      </div>
-      <div>
-        <div>Slot:</div>
-        <div>
-          <div>{nf.format(slot)}</div>
+          <div>{blockDatetime.date}</div>
+          <div>{blockDatetime.time}</div>
         </div>
       </div>
       <div>
@@ -44,11 +44,19 @@ const GeneralInfo = ({
         <div>Recent Blockhash</div>
         <ByteString input={message.recentBlockhash} />
       </div>
+      {transaction.transaction.signatures.length !== 1 && (
+        <div>
+          <div>Signatures</div>
+          {transaction.transaction.signatures.map((o) => (
+            <ByteString input={o} key={o} />
+          ))}
+        </div>
+      )}
       <div>
-        <div>Signatures</div>
-        {transaction.transaction.signatures.map((o) => (
-          <ByteString input={o} key={o} />
-        ))}
+        <div>Slot:</div>
+        <div>
+          <div>{nf.format(slot)}</div>
+        </div>
       </div>
       {message.addressTableLookups && (
         <div>
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -4,6 +4,3 @@ export const nf = new Intl.NumberFormat();
 
 export const toSol = (lamports: number) =>
   nf.format(lamports / LAMPORTS_PER_SOL);
-
-export const blockTimeToISO = (blockTime = 0) =>
-  new Date(blockTime * 1000).toISOString();
