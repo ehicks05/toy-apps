a1be6907e00d6096bb5ce7e5d5f774604a163f54
Eric Hicks <ehicks05@yahoo.com>
2023-05-13 13:27:12 -0400

    update url to use our local copies of the data files

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,5 @@
-const usConfirmedUrl =
-  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
-const usDeathsUrl =
-  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv";
+const usConfirmedUrl = "/data/time_series_covid19_confirmed_US.csv";
+const usDeathsUrl = "/data/time_series_covid19_deaths_US.csv";
 
 const INFECTION_DURATION = 14;
 
