6845160469dee9ac81e79b99af7be0775c307309
Eric Hicks <ehicks05@yahoo.com>
2023-09-20 22:05:25 -0400

    add color coding of pleasant days

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import React, {
-  Fragment,
-  Ref,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
-import {
-  Circle,
-  MapContainer,
-  Marker,
-  Polygon,
-  Popup,
-  TileLayer,
-} from 'react-leaflet';
-import { LatLngExpression, Map } from 'leaflet';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Circle, MapContainer, Polygon, Popup, TileLayer } from 'react-leaflet';
+import { Map } from 'leaflet';
 import Voronoi from 'voronoi';
 import { useQueryParams } from 'use-query-params';
+import { sortBy } from 'lodash';
 import { Header, Footer } from './core-components';
 import stationData from './us_stations.json';
 import { QUERY_PARAMS } from './queryParams';
 
-const zoom = 12;
+const zoom = 5;
 
 const purpleOptions = { color: 'purple' };
 
@@ -72,6 +59,10 @@ function DisplayPosition({ map }: { map: Map }) {
 function App() {
   const [map, setMap] = useState<Map | null>(null);
   const [params, setParams] = useQueryParams(QUERY_PARAMS);
+
+  const matchStationToDiagram = ({ x, y }: { x: number; y: number }) =>
+    stationData.find(s => s.longitude === x && s.latitude === y);
+
   const displayMap = useMemo(
     () => (
       <MapContainer
@@ -88,68 +79,75 @@ function App() {
         {stationData
           .filter(s => s.name.endsWith(' US'))
           .map(station => (
-            <Fragment key={station.station}>
-              <Circle
-                center={[station.latitude, station.longitude]}
-                radius={(15 / (map?.getZoom() || 1)) * 2000}
-                pathOptions={purpleOptions}
-              />
-              {/* <Marker position={[station.latitude, station.longitude]}>
-              <Popup>
-                {station.station}
-                <br />
-                {station.name}
-              </Popup>
-            </Marker> */}
-            </Fragment>
-          ))}
-        {diagram.cells.map(cell => (
-          <>
-            <Polygon
-              center={[cell.site.y, cell.site.x]}
-              positions={cell.halfedges.flatMap(halfedge => [
-                [halfedge.getStartpoint().y, halfedge.getStartpoint().x],
-                [halfedge.getEndpoint().y, halfedge.getEndpoint().x],
-              ])}
-              radius={3000}
+            <Circle
+              key={station.id}
+              center={[station.latitude, station.longitude]}
+              radius={(15 / (map?.getZoom() || 1)) * 300}
               pathOptions={purpleOptions}
+              weight={0.1}
             />
-            {/* <Marker position={[cell.site.y, cell.site.x]}>
-              <Popup>
-                yo
-                <br />
-                yo
-              </Popup>
-            </Marker> */}
-          </>
-        ))}
+          ))}
+        {diagram.cells.map(cell => {
+          const station = matchStationToDiagram(cell.site);
+          const pleasant_days = station?.pleasant_days || 0;
+          const hue = (pleasant_days * 255) / 366;
+          return (
+            <React.Fragment key={`${cell.site.x},${cell.site.y}`}>
+              <Polygon
+                center={[cell.site.y, cell.site.x]}
+                positions={cell.halfedges.flatMap(halfedge => [
+                  [halfedge.getStartpoint().y, halfedge.getStartpoint().x],
+                  [halfedge.getEndpoint().y, halfedge.getEndpoint().x],
+                ])}
+                radius={3000}
+                weight={1}
+                fillColor={`hsl(${hue}, 100%, 50%)`}
+                fillOpacity={0.3}
+              >
+                <Popup>
+                  {station.id}
+                  <br />
+                  {station.name} - {station.pleasant_days}
+                </Popup>
+              </Polygon>
+            </React.Fragment>
+          );
+        })}
       </MapContainer>
     ),
-    [map],
+    [map, params.lat, params.long],
   );
-
-  // const halfedge = diagram.cells[0].halfedges[0];
 
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-tr from-indigo-900 to-green-900 text-gray-50">
       <Header />
-      {/* <pre className="text-xs">
-        {JSON.stringify(diagram.cells[0].halfedges[0].getStartpoint(), null, 2)}
-      </pre> */}
-      {/* <pre className="text-xs">
-        {JSON.stringify(
-          [
-            [halfedge.getStartpoint().y, halfedge.getStartpoint().x],
-            [halfedge.getEndpoint().y, halfedge.getEndpoint().x],
-          ],
-          null,
-          2,
-        )}
-      </pre>
-      <pre className="text-xs">{JSON.stringify(diagram, null, 2)}</pre> */}
       <div className="flex h-full flex-grow flex-col pb-4 sm:px-4">
         {displayMap}
         {map ? <DisplayPosition map={map} /> : null}
+      </div>
+      <div>
+        <table cellPadding={3}>
+          <thead>
+            <tr>
+              <td />
+              <td>Station</td>
+              <td>State</td>
+              <td>Pleasant Days</td>
+            </tr>
+          </thead>
+          <tbody>
+            {sortBy(stationData, o => o.pleasant_days)
+              .reverse()
+              .map((s, i) => (
+                <tr key={s.id}>
+                  <td className="text-right">{i + 1}. </td>
+                  <td>{s.name.split(',')[0]}</td>
+                  <td>{s.name.split(' ')[s.name.split(' ').length - 2]}</td>
+                  <td className="text-right">{s.pleasant_days}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
       <Footer />
     </div>
