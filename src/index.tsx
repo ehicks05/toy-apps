398dc1f62c188e3228d9edd8c7cf6ebeda379135
Eric Hicks <ehicks05@yahoo.com>
2023-11-29 23:17:58 -0500

    add react query and date-fns-tz

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
-import './index.css';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ModalProvider } from 'react-modal-hook';
+import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider } from "react-modal-hook";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from './MyApp';
+import App from "./MyApp";
 
-const container = document.getElementById('root');
+const queryClient = new QueryClient();
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <BrowserRouter>
     <ModalProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ModalProvider>
   </BrowserRouter>
 );
