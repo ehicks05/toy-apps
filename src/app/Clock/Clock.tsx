3af7c2f97ac7cb8dd86fdba7e14a85ced99b5728
Eric Hicks <ehicks05@yahoo.com>
2023-12-02 16:10:15 -0500

    show relative offset instead of utc offset

--- src/app/Clock/Clock.tsx
+++ src/app/Clock/Clock.tsx
@@ -7,10 +7,35 @@ import { Geoname } from "@/services/geonames";
 import { useLocalStorage } from "usehooks-ts";
 import { getTimeParts } from "./utils";
 import CityDialog from "./CityDialog";
+import { formatInTimeZone, getTimezoneOffset } from "date-fns-tz";
+import { format } from "date-fns";
 
 const FONT_SIZES = {
   PRIMARY: "text-8xl sm:text-9xl",
   SECONDARY: "text-3xl sm:text-4xl md:text-5xl",
+};
+
+const getRelativeOffset = (timeZoneId: string) => {
+  const offsetMillis =
+    getTimezoneOffset(timeZoneId) -
+    getTimezoneOffset(Intl.DateTimeFormat().resolvedOptions().timeZone);
+  const hour = offsetMillis / 1000 / 60 / 60;
+  const minute = (offsetMillis / 1000 / 60) % 60;
+  const offset = `${offsetMillis > 0 ? "+" : "-"}${hour + "h"}${
+    minute ? ":" + minute : ""
+  }`;
+
+  const localDate = format(new Date(), "dd");
+  const nonLocalDate = formatInTimeZone(new Date(), timeZoneId, "dd");
+
+  const relativeDate =
+    nonLocalDate > localDate
+      ? " tomorrow"
+      : nonLocalDate < localDate
+        ? " yesterday"
+        : "";
+
+  return `${offset}${relativeDate}`;
 };
 
 const City = ({ city }: { city: Geoname }) => {
@@ -19,7 +44,8 @@ const City = ({ city }: { city: Geoname }) => {
     <div className="w-full flex justify-between items-center gap-4 p-4 bg-slate-900 rounded-lg">
       <div className="flex flex-col">
         <div className="text-xl">{city.name}</div>
-        <div className="text-sm">{offset}</div>
+        <div className="text-sm" title={`utc offset: ${offset}`}>
+          {getRelativeOffset(city.timeZoneId)}</div>
       </div>
       <div className="flex items-baseline gap-1">
         <div className={FONT_SIZES.SECONDARY}>{time}</div>
@@ -33,10 +59,7 @@ export const Clock = () => {
   const { date: _date } = useClock();
   const { time, ampm, date } = getTimeParts(_date);
 
-  const [selectedCities] = useLocalStorage<Geoname[]>(
-    "selectedCities",
-    []
-  );
+  const [selectedCities] = useLocalStorage<Geoname[]>("selectedCities", []);
 
   const [isCityDialogOpen, setIsCityDialogOpen] = useState(false);
 
