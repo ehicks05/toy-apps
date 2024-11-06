365fa09c115fe5c8cca0264e1765b87630201d38
Eric Hicks <ehicks05@yahoo.com>
2024-11-06 18:43:44 -0500

    sort partial days after

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -3,8 +3,22 @@ import { useLocalStorage } from '@uidotdev/usehooks';
 import { Temporal } from 'temporal-polyfill';
 import type { Event } from '../components/Calendar/types';
 
-const sort = (a: Event, b: Event) =>
-	Temporal.ZonedDateTime.compare(a.start, b.start);
+const isPartialDay = (event: Event) =>
+	!event.isAllDay && event.start.toPlainDate().equals(event.end.toPlainDate());
+
+const partialDaySort = (a: Event, b: Event) => {
+	const a1 = isPartialDay(a) ? -1 : 1;
+	const b1 = isPartialDay(b) ? -1 : 1;
+	return b1 - a1;
+};
+
+const sort = (a: Event, b: Event) => {
+	const partialDayResult = partialDaySort(a, b);
+	if (partialDayResult !== 0) return partialDayResult;
+
+	const startResult = Temporal.ZonedDateTime.compare(a.start, b.start);
+	return startResult;
+};
 
 const hydrateEvent = (e: Event) => ({
 	...e,
@@ -13,9 +27,9 @@ const hydrateEvent = (e: Event) => ({
 });
 
 export const useEvents = () => {
-	const [_events, setEvents] = useLocalStorage('ecal-events', EVENTS.toSorted(sort));
+	const [_events, setEvents] = useLocalStorage('ecal-events', EVENTS);
 
-	const events = _events.map(hydrateEvent);
+	const events = _events.map(hydrateEvent).toSorted(sort);
 
 	return {
 		events,
