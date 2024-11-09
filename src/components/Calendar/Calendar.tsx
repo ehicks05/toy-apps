00fe761972678839e06ba3f5ba744b483c7a74cc
Eric Hicks <ehicks05@yahoo.com>
2024-11-09 08:54:09 -0500

    refactor wip

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -8,6 +8,7 @@ import { MonthMenu } from './MonthMenu';
 import { WeekdayNames } from './WeekdayNames';
 import { MyDndContext } from './dnd/DndContext';
 import { isOverlapsDay } from './events';
+import type { Event } from './types';
 import { calendarDaysForMonth } from './utils/calendarDaysForMonth';
 
 /*
@@ -17,30 +18,82 @@ import { calendarDaysForMonth } from './utils/calendarDaysForMonth';
  2a. if start dates match, later end dates should be sorted earlier
  */
 
-const CalendarWeeks = () => {};
+interface CalendarWeekProps {
+	days: Temporal.ZonedDateTime[];
+	eventsInWeek: Event[];
+}
+
+const CalendarWeek = ({ days, eventsInWeek }: CalendarWeekProps) => {
+	// find each event's 'swimlane'
+	const eventLanes: Record<string, number> = {};
+	const eventDayMasks: Record<string, number[]> = {};
+	const dailyOffsets = [0, 0, 0, 0, 0, 0, 0];
+
+	days.forEach((day, i) => {
+		const eventsInDay = eventsInWeek.filter(
+			(event) =>
+				event.start.toPlainDate().equals(day.toPlainDate()) ||
+				(i === 0 &&
+					Temporal.PlainDate.compare(event.start.toPlainDate(), day.toPlainDate()) <
+						0),
+		);
+
+		eventsInDay.forEach((event) => {
+			const offset = dailyOffsets[i];
+			const offsetMovingForward = offset + 1;
+			eventLanes[event.id] = offset;
+			eventDayMasks[event.id] = event.dayMask;
+
+			// update daily offsets
+			dailyOffsets.forEach((offset, i) => {
+				dailyOffsets[i] = event.dayMask[i] ? offsetMovingForward : dailyOffsets[i];
+			});
+		});
+	});
+
+	return days.map((date, i) => {
+		const eventsInDay = eventsInWeek
+			.filter(
+				(event) =>
+					event.start.toPlainDate().equals(date.toPlainDate()) ||
+					(i === 0 &&
+						Temporal.PlainDate.compare(
+							event.start.toPlainDate(),
+							date.toPlainDate(),
+						) < 0),
+			)
+			.map((event) => ({
+				...event,
+				lane: eventLanes[event.id],
+				days: sum(eventDayMasks[event.id]),
+			}));
+
+		return <Day key={date.toString()} date={date} eventsInDay={eventsInDay} />;
+	});
+};
+
+const getMonthLabel = (date: Temporal.ZonedDateTime) =>
+	date.toLocaleString('en-US', {
+		month: 'long',
+		year: 'numeric',
+	});
 
 interface Props {
 	date: Temporal.ZonedDateTime;
 }
 
 export const Calendar = ({ date: _date }: Props) => {
+	const [date, setDate] = useState(_date);
 	const { isShowWeekend } = useSettings();
 	const { events } = useEvents();
 	const cols = isShowWeekend ? 7 : 5;
-
-	const [date, setDate] = useState(_date);
-
 	const gridCols = cols === 7 ? 'grid-cols-7' : 'grid-cols-5';
-	const monthLabel = date.toLocaleString('en-US', {
-		month: 'long',
-		year: 'numeric',
-	});
 
 	const days = calendarDaysForMonth(date).filter(
 		(date) => isShowWeekend || ![0, 6].includes(date.dayOfWeek),
 	);
 
-	const weeks = chunk(days, cols).map((week) => {
+	const weeks = chunk(days, cols).map((week, i) => {
 		const eventsInWeek = events
 			.map((event) => {
 				const dayMask = [0, 0, 0, 0, 0, 0, 0];
@@ -51,10 +104,8 @@ export const Calendar = ({ date: _date }: Props) => {
 				return { ...event, dayMask };
 			})
 			.filter((event) => event.dayMask.some((dayValue) => dayValue !== 0));
-		return { days: week, eventsInWeek };
+		return { index: i, days: week, eventsInWeek };
 	});
-
-	console.log(weeks);
 
 	return (
 		<MyDndContext>
@@ -62,67 +113,16 @@ export const Calendar = ({ date: _date }: Props) => {
 				<div className="p-2 border border-neutral-800 bg-neutral-800 font-semibold text-center">
 					<div className="flex justify-between items-center">
 						<div className="w-32" /> {/* hack to keep label centered */}
-						{monthLabel}
+						{getMonthLabel(date)}
 						<MonthMenu date={date} setDate={setDate} _date={_date} />
 					</div>
 				</div>
 				<div className={`w-full grid ${gridCols}`}>
 					<WeekdayNames />
 
-					{weeks.map(({ days, eventsInWeek }) => {
-						// find each event's 'swimlane'
-						const eventLanes: Record<string, number> = {};
-						const eventDayMasks: Record<string, number[]> = {};
-						const dailyOffsets = [0, 0, 0, 0, 0, 0, 0];
-
-						days.forEach((day, i) => {
-							const eventsInDay = eventsInWeek.filter(
-								(event) =>
-									event.start.toPlainDate().equals(day.toPlainDate()) ||
-									(i === 0 &&
-										Temporal.PlainDate.compare(
-											event.start.toPlainDate(),
-											day.toPlainDate(),
-										) < 0),
-							);
-
-							eventsInDay.forEach((event) => {
-								const offset = dailyOffsets[i];
-								const offsetMovingForward = offset + 1;
-								eventLanes[event.id] = offset;
-								eventDayMasks[event.id] = event.dayMask;
-
-								// update daily offsets
-								dailyOffsets.forEach((offset, i) => {
-									dailyOffsets[i] = event.dayMask[i]
-										? offsetMovingForward
-										: dailyOffsets[i];
-								});
-							});
-						});
-
-						return days.map((date, i) => {
-							const eventsInDay = eventsInWeek
-								.filter(
-									(event) =>
-										event.start.toPlainDate().equals(date.toPlainDate()) ||
-										(i === 0 &&
-											Temporal.PlainDate.compare(
-												event.start.toPlainDate(),
-												date.toPlainDate(),
-											) < 0),
-								)
-								.map((event) => ({
-									...event,
-									lane: eventLanes[event.id],
-									days: sum(eventDayMasks[event.id]),
-								}));
-
-							return (
-								<Day key={date.toString()} date={date} eventsInDay={eventsInDay} />
-							);
-						});
-					})}
+					{weeks.map(({ index, days, eventsInWeek }) => (
+						<CalendarWeek key={index} days={days} eventsInWeek={eventsInWeek} />
+					))}
 				</div>
 			</div>
 		</MyDndContext>
