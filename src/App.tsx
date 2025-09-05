674d52fe74a2fa5d93eec0f383490150e8fe1b14
Eric Hicks <ehicks05@yahoo.com>
2025-09-05 12:34:56 -0400

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import MyApp from './jikan/MyApp';
 
+const MainMenu = () => {
+	return (
+		<div>
+			<Link to={'/jikan'}>jikan - clock, timer, stopwatch</Link>
+		</div>
+	);
+};
+
 export function App() {
 	return (
 		<div className="flex flex-col min-h-screen bg-gradient-to-r from-sky-950 to-blue-950">
-			<div><Link to="/">home</Link></div>
+			<div>
+				<Link to="/">home</Link>
+			</div>
 			<div className="flex-grow flex flex-col h-full sm:px-4">
 				<Routes>
-					<Route
-						path="/"
-						element={
-							<div>
-								<Link to={'/jikan'}>jikan - clock, timer, stopwatch</Link>
-							</div>
-						}
-					/>
+					<Route path="/" element={<MainMenu />} />
 					<Route path="/jikan/*" element={<MyApp />} />
 				</Routes>
 			</div>
