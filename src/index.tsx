a3641715d64296f5e3c5b909e1d6aa73820d5f21
Eric Hicks <ehicks05@yahoo.com>
2025-09-05 12:17:06 -0400

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
-import { ModalProvider } from 'react-modal-hook';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
@@ -11,10 +10,8 @@ const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
 root.render(
 	<BrowserRouter>
-		<ModalProvider>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</ModalProvider>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</BrowserRouter>,
 );
