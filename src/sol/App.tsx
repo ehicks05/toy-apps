5b3ad435348af43cab85a11b46f47e94aad6cdec
Eric Hicks <ehicks05@yahoo.com>
2025-09-11 13:16:32 -0400

    cleanup

--- src/sol/App.tsx
+++ src/sol/App.tsx
@@ -1,19 +1,19 @@
-import { Wallet as WalletProvider } from './components';
 import { Home } from './app/index';
-import { Footer, Header } from './components';
+import { Footer, Header, Wallet as WalletProvider } from './components';
 import './index.css';
 
 export function Sol() {
 	document.title = 'Sol';
 
 	return (
-		    <WalletProvider>
-<div className="flex min-h-screen flex-col bg-gradient-to-tr from-violet-900 to-gray-900 text-gray-50">
-			<Header />
-			<div className="flex h-full flex-grow flex-col p-4 pt-0">
-				<Home />
+		<WalletProvider>
+			<div className="flex min-h-screen flex-col bg-gradient-to-tr from-violet-900 to-gray-900 text-gray-50">
+				<Header />
+				<div className="flex h-full flex-grow flex-col p-4 pt-0">
+					<Home />
+				</div>
+				<Footer />
 			</div>
-			<Footer />
-		</div></WalletProvider>
+		</WalletProvider>
 	);
 }
