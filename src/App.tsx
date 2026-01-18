de7eda1002f06bd45c15c209f533a431c007753b
Eric Hicks <ehicks05@yahoo.com>
2026-01-18 12:59:24 -0500

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Home, Sun, Wine } from 'lucide-react';
+import { FaBlackTie } from 'react-icons/fa';
 import { Link, Route, Routes } from 'react-router-dom';
 import { CalElDemo } from './cal-el/src/CalElDemo';
 import { CovidCharts } from './covid/CovidCharts';
 import Eordle from './eordle/Eordle';
 import { EricIdle } from './eric-idle/EricIdle';
+import { useFavicon } from './hooks/useFavicon';
 import { Hootch } from './hootch';
 import Jikan from './jikan/Jikan';
 import { Jobbies } from './jobbies';
@@ -12,34 +15,126 @@ import { UnitedStatesOfFood as FoodMap } from './united-states-of-food';
 import { Weatherman } from './weatherman';
 
 const APPS = [
-	{ name: 'jikan', description: 'clock, timer, stopwatch', component: Jikan },
-	{ name: 'eordle', description: 'wordle clone', component: Eordle },
-	{ name: 'hootch', description: 'booze math', component: Hootch },
-	{ name: 'covid-charts', description: 'charts and tables', component: CovidCharts },
-	{ name: 'cal-el', description: 'calendar', component: CalElDemo },
+	{
+		name: 'cal-el',
+		description: 'calendar',
+		component: CalElDemo,
+		iconUrl: '/cal-el/android-chrome-512x512.png',
+	},
+	{
+		name: 'eordle',
+		description: 'wordle clone',
+		component: Eordle,
+		iconUrl: '/eordle/icon.png',
+	},
+	{
+		name: 'covid-charts',
+		description: 'charts and tables',
+		component: CovidCharts,
+		iconUrl: '/covid/android-chrome-512x512.png',
+	},
 	{
 		name: 'logical-fitness',
 		description: 'fitness levels',
 		component: LogicalFitness,
+		iconUrl: '/logical-fitness/android-chrome-512x512.png',
 	},
-	{ name: 'eric-idle', description: 'idle game', component: EricIdle },
-	{ name: 'food-map', description: 'map of state foods', component: FoodMap },
-	{ name: 'jobbies', description: 'job app tracker', component: Jobbies },
-	{ name: 'sol', description: 'play with solana apis', component: Sol },
-	{ name: 'weatherman', description: 'nice climate map', component: Weatherman },
+	{
+		name: 'eric-idle',
+		description: 'idle game',
+		component: EricIdle,
+		iconUrl: '/eric-idle/favicon.ico',
+	},
+	{
+		name: 'jobbies',
+		description: 'job app tracker',
+		component: Jobbies,
+		icon: <FaBlackTie className="size-full" />,
+	},
+	{
+		name: 'weatherman',
+		description: 'nice climate map',
+		component: Weatherman,
+		iconUrl: '/weatherman/favicon.ico',
+	},
+	{
+		name: 'jikan',
+		description: 'clock, timer, stopwatch',
+		component: Jikan,
+		iconUrl: '/jikan/icon-color.svg',
+	},
+	{
+		name: 'sol',
+		description: 'play with solana apis',
+		component: Sol,
+		icon: <Sun className="size-full text-yellow-500" />,
+	},
+	{
+		name: 'food-map',
+		description: 'map of state foods',
+		component: FoodMap,
+		iconUrl: '/united-states-of-food/favicon.ico',
+	},
+	{
+		name: 'hootch',
+		description: 'booze math',
+		component: Hootch,
+		icon: <Wine className="size-full text-indigo-600" />,
+	},
 ];
+
+interface Props {
+	name: string;
+	description: string;
+	iconUrl?: string;
+	icon?: React.ReactNode;
+}
+
+const AppItem = ({ name, description, iconUrl, icon }: Props) => {
+	return (
+		<Link key={name} to={`/${name}`}>
+			<div className="flex gap-2 items-center p-2 border border-neutral-700 rounded-lg">
+				<div className="size-8">
+					{iconUrl && <img src={iconUrl} alt="icon" className="size-full rounded" />}
+					{icon && icon}
+				</div>
+				<div>
+					<div>{name}</div>
+					<div>{description}</div>
+				</div>
+			</div>
+		</Link>
+	);
+};
 
 const MainMenu = () => {
 	document.title = 'toy apps';
+	useFavicon('/eordle/icon.png');
 
 	return (
-		<div className="flex flex-col">
+		<div className="grid grid-cols-3 gap-2 p-2">
 			{APPS.map((app) => (
-				<Link key={app.name} to={`/${app.name}`}>
-					{app.name} - {app.description}
-				</Link>
+				<AppItem
+					key={app.name}
+					name={app.name}
+					description={app.description}
+					iconUrl={app.iconUrl}
+					icon={app.icon}
+				/>
 			))}
 		</div>
+	);
+};
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<MainMenu />} />
+
+			{APPS.map((app) => (
+				<Route key={app.name} path={`/${app.name}/*`} element={<app.component />} />
+			))}
+		</Routes>
 	);
 };
 
@@ -47,20 +142,12 @@ export function App() {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<div className="flex-grow flex flex-col h-full">
-				<Routes>
-					<Route path="/" element={<MainMenu />} />
-
-					{APPS.map((app) => (
-						<Route
-							key={app.name}
-							path={`/${app.name}/*`}
-							element={<app.component />}
-						/>
-					))}
-				</Routes>
+				<AppRoutes />
 			</div>
-			<div>
-				<Link to="/">Home</Link>
+			<div className="p-2">
+				<Link to="/" className="flex gap-1">
+					<Home /> Home
+				</Link>
 			</div>
 		</div>
 	);
